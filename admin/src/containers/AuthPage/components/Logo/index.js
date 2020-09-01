f5a8e9c8d734eb1600c58adf667d7990d96b0a0e
srlechuga18 <aledeleon222@hotmail.com>
2020-09-01 16:07:32 -0500

    ui admin

---
+++ admin/src/containers/AuthPage/components/Logo/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import LogoRCG from '../../../../assets/RCG/imgs/Logo_negro.png';
+import Img from './Img';
+
+const Logo = () => <Img src={LogoRCG} alt="strapi-logo" />;
+
+export default Logo;
