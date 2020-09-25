e9a493126e356406fae2206a363133205a90a4ef
Iojioji <erfamel@hotmail.com>
2020-09-25 17:43:17 -0500

    Send email implementation for calca and contacto

--- api/calca/controllers/calca.js
+++ api/calca/controllers/calca.js
@@ -5,4 +5,38 @@
  * to customize this controller
  */
 
-module.exports = {};
+ const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+    async create(ctx) {
+        let entity;
+        if (ctx.is('multipart')) {
+            const {data, files} = parseMultipartData(ctx);
+            entity = await strapi.services.calca.create(data, {files});
+        }
+        else
+        {
+            entity = await strapi.services.calca.create(ctx.request.body);
+        }
+
+        let entry = sanitizeEntity(entity, {model: strapi.models.calca});
+
+        console.log("Adding calca:\r\n", entry);
+
+        strapi.plugins['email'].services.email.send({
+            to: "erfamel@gmail.com",
+            from: "about@wetheforce.com",
+            subject: "Se ha dado de alta una calca",
+            text: "Se ha dado de alta una calca",
+            html: `<h2>Alguien ha dado de alta una calca</h2>
+            <br>
+            <p><b>Nombre:</b> ${entry.nombre}</p>
+            <p><b>Direccion:</b> ${entry.direccion}</p>
+            <p><b>Ciudad:</b> ${entry.ciudad}</p>
+            <p><b>Telefono:</b> ${entry.telefono}</p>
+            <p><b>Calca:</b> ${entry.numero_calca}</p>`
+        })
+
+        return entry;
+    }
+};
