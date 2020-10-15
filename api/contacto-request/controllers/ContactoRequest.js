9ee7794d1d35811e38c4e16a507d2603afb3ba96
srlechuga18 <aledeleon222@hotmail.com>
2020-10-15 18:24:38 -0500

    correo await

--- api/contacto-request/controllers/ContactoRequest.js
+++ api/contacto-request/controllers/ContactoRequest.js
@@ -1,13 +1,9 @@
 module.exports = {
     // POST /request
     req: async ctx => {
-        let requestObject = ctx.request.body.requestObject;
-        console.log('Ayayay test');
-        console.log("request thing", ctx.request.body);
-
-        console.log("Sending email");
-
-        strapi.plugins['email'].services.email.send({
+        let requestObject = ctx.request.body;
+        //await strapi.plugins['email'].services.email.send({
+        await strapi.plugins['email'].services.email.send({
             to: "erfamel@gmail.com",
             from: "about@wetheforce.com",
             subject: `Intento de Contacto [${requestObject.subject}]`,
@@ -19,7 +15,7 @@ module.exports = {
             <p><b>Asunto:</b> ${requestObject.subject}</p>
             <p><b>Mensaje:</b> ${requestObject.message}</p>`
         });
-        return `Ayyy si se pudo como no`;
+        return '';
 
         /*
             <h2>Alguien se ha intentado poner en contacto con ustedes</h2>
