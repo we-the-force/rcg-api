5fe837e3ecf047e6e91f002f59b30c70dba2983c
srlechuga18 <aledleon2@gmail.com>
2021-05-25 17:14:28 -0500

    quitar/comentar la validacion de fotos para el banner

--- middlewares/validateArticulo/index.js
+++ middlewares/validateArticulo/index.js
@@ -1,75 +1,75 @@
 module.exports = strapi => {
     return {
         initialize() {
-            strapi.app.use(async (ctx, next) => {
-                if (ctx.method == 'POST' || ctx.method == "PUT")
-                {
-                    // console.log("Soy un url:\r\n", ctx.url);
-                    let urlThings = (ctx.url).split("/");
-                    // console.log("Cosas del url:\r\n", urlThings);
-                    if (urlThings.length >= 4)
-                    {
-                        if (urlThings[1] == "content-manager" && urlThings[3] == "application::articulo.articulo")
-                        {
-                            if (ctx.request.body.data != undefined)
-                            {
-                                let result = {
-                                    valid: true,
-                                    errorMessage: "",
-                                    errors: 0
-                                }
-    
-                                let body = JSON.parse(ctx.request.body.data);
-    
-                                if (body.cover === undefined || body.cover === null)
-                                {
-                                    result.valid = false;
-                                    result.errors++;
-                                    result.errorMessage += "Tener cover es obligatorio";
-                                }
-                                else
-                                {
-                                    let coverData = await strapi.plugins.upload.services.upload.fetch({id: body.cover});
-                                    coverData = {
-                                        width: coverData.width,
-                                        height: coverData.height
-                                    };
-                                    console.log("coverData: ", coverData);
-    
-                                    if (coverData.width < 1280 || coverData.height < 720)
-                                    {
-                                        result.valid = false;
-                                        result.errors++;
-                                        result.errorMessage += `La resolucion de la imagen no cumple los requisitos minimos (tiene [${coverData.width}x${coverData.height}] y se requiere [1280x720])`;
-                                    }
-                                }
-                                if (body.autor === undefined || body.autor === null)
-                                {
-                                    result.valid = false;
-                                    result.errors++;
-                                    result.errorMessage += result.errors > 0 ? ", el articulo debe tener un autor relacionado" : "El articulo debe tener un autor relacionado";
-                                }
-                                if (body.categoria === undefined || body.categoria === null)
-                                {
-                                    result.valid = false;
-                                    result.errors++;
-                                    result.errorMessage += result.errors > 0 ? ", el articulo debe tener una categoria relacionada" : "El articulo debe tener una categoria relacionada";
-                                }
-    
-                                if (!result.valid)
-                                {
-                                    // console.log(`Ocurrio un error publicando el articulo: \r\n${result.errorMessage}`);
-                                    // alert(`Ocurrio un error publicando el articulo: \r\n${result.errorMessage}`);
-                                    // strapi.notification.error(`Ocurrio un error publicando el articulo: \r\n${result.errorMessage}`);
-                                    // return "";
-                                    return ctx.throw(400, `Error: ${result.errorMessage}`);
-                                    // throw new Error(`Ocurrio un error publicando el articulo: \r\n${result.errorMessage}`);
-    
-                                }
-                            }
-                        }
-                    }
-                }
+            strapi.app.use(async(ctx, next) => {
+                // if (ctx.method == 'POST' || ctx.method == "PUT")
+                // {
+                //     // console.log("Soy un url:\r\n", ctx.url);
+                //     let urlThings = (ctx.url).split("/");
+                //     // console.log("Cosas del url:\r\n", urlThings);
+                //     if (urlThings.length >= 4)
+                //     {
+                //         if (urlThings[1] == "content-manager" && urlThings[3] == "application::articulo.articulo")
+                //         {
+                //             if (ctx.request.body.data != undefined)
+                //             {
+                //                 let result = {
+                //                     valid: true,
+                //                     errorMessage: "",
+                //                     errors: 0
+                //                 }
+
+                //                 let body = JSON.parse(ctx.request.body.data);
+
+                //                 if (body.cover === undefined || body.cover === null)
+                //                 {
+                //                     result.valid = false;
+                //                     result.errors++;
+                //                     result.errorMessage += "Tener cover es obligatorio";
+                //                 }
+                //                 else
+                //                 {
+                //                     let coverData = await strapi.plugins.upload.services.upload.fetch({id: body.cover});
+                //                     coverData = {
+                //                         width: coverData.width,
+                //                         height: coverData.height
+                //                     };
+                //                     console.log("coverData: ", coverData);
+
+                //                     if (coverData.width < 1280 || coverData.height < 720)
+                //                     {
+                //                         result.valid = false;
+                //                         result.errors++;
+                //                         result.errorMessage += `La resolucion de la imagen no cumple los requisitos minimos (tiene [${coverData.width}x${coverData.height}] y se requiere [1280x720])`;
+                //                     }
+                //                 }
+                //                 if (body.autor === undefined || body.autor === null)
+                //                 {
+                //                     result.valid = false;
+                //                     result.errors++;
+                //                     result.errorMessage += result.errors > 0 ? ", el articulo debe tener un autor relacionado" : "El articulo debe tener un autor relacionado";
+                //                 }
+                //                 if (body.categoria === undefined || body.categoria === null)
+                //                 {
+                //                     result.valid = false;
+                //                     result.errors++;
+                //                     result.errorMessage += result.errors > 0 ? ", el articulo debe tener una categoria relacionada" : "El articulo debe tener una categoria relacionada";
+                //                 }
+
+                //                 if (!result.valid)
+                //                 {
+                //                     // console.log(`Ocurrio un error publicando el articulo: \r\n${result.errorMessage}`);
+                //                     // alert(`Ocurrio un error publicando el articulo: \r\n${result.errorMessage}`);
+                //                     // strapi.notification.error(`Ocurrio un error publicando el articulo: \r\n${result.errorMessage}`);
+                //                     // return "";
+                //                     return ctx.throw(400, `Error: ${result.errorMessage}`);
+                //                     // throw new Error(`Ocurrio un error publicando el articulo: \r\n${result.errorMessage}`);
+
+                //                 }
+                //             }
+                //         }
+                //     }
+                // }
                 await next();
             })
         }
