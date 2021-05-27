0ded1cb775d04c9ed9f04a3110d399f9658bd5c9
srlechuga18 <aledleon2@gmail.com>
2021-05-27 12:38:58 -0500

    migration to 3.2.3

--- config/server.js
+++ config/server.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  url: env("PUBLIC_URL", "http://149.28.252.152:1337"),
-  port: env.int("PORT", 1337),
-  admin: {
-    auth: {
-      secret: env("ADMIN_JWT_SECRET", "9867d76d936cabf46b7333c627425e40"),
+    host: env("HOST", "localhost"),
+    url: env("PUBLIC_URL", ""),
+    port: env.int("PORT", 1337),
+    admin: {
+        auth: {
+            secret: env("ADMIN_JWT_SECRET", "9867d76d936cabf46b7333c627425e40"),
+        },
     },
-  },
-  cron: {
-    enabled: true,
-  },
+    cron: {
+        enabled: true,
+    },
 });
