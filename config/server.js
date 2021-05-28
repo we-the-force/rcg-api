72994ed86466a0bc168587fbd6dec543f08879f4
srlechuga18 <aledleon2@gmail.com>
2021-05-28 17:47:28 -0500

    host change

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-    host: env("HOST", "localhost"),
+    host: env("HOST", "0.0.0.0"),
     url: env("PUBLIC_URL", "https://api.rcgmedia.mx"),
     port: env.int("PORT", 1337),
     admin: {
