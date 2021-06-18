afe2107f64f16279062b0a70b1473ac7e87b2ac5
srlechuga18 <aledleon2@gmail.com>
2021-06-18 17:55:43 -0500

    test limit articulo

--- api/articulo/controllers/articulo.js
+++ api/articulo/controllers/articulo.js
@@ -1,13 +1,35 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
-const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
-    // async update(ctx) {
-    //     console.log("Updating articulo");
-    // }
+  async find(ctx) {
+    let entities;
+    ctx.query = {
+      ...ctx.query,
+      _limit: 20,
+    };
+    if (ctx.query._q) {
+      entities = await strapi.services.restaurant.search(ctx.query);
+    } else {
+      entities = await strapi.services.restaurant.find(ctx.query);
+    }
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.restaurant })
+    );
+  },
+
+  // async find(ctx) {
+  //     let entities;
+
+  //     // What you need to inject
+  //     ctx.query = {
+  //     ...ctx.query,
+  //     _limit: 20,
+  // },
 };
