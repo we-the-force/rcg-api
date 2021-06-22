265d472aded32317a2a732a00290b875313e3e7f
srlechuga18 <aledleon2@gmail.com>
2021-06-22 17:17:29 -0500

    omit url

--- api/articulo/models/articulo.js
+++ api/articulo/models/articulo.js
@@ -17,7 +17,6 @@ module.exports = {
                 app_id: "2b8f51fa-8098-49d8-a9a5-a36441f41907",
                 headings: { "en": "RCG Media" },
                 contents: { "en": data.Title },
-                url: 'https://rcgmedia.mx/articulo/' + data.url + '/',
                 web_url: 'https://rcgmedia.mx/articulo/' + data.url + '/',
                 app_url: 'https://rcgmedia.mx/articulo/' + data.url + '/',
                 small_icon: "https://api.rcgmedia.mx/uploads/logo_rcg_media_250_1_69610a5309.png",
