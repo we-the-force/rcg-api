d6e095e86d730e8e46fad04ce2129081004b497f
srlechuga18 <aledleon2@gmail.com>
2021-07-02 12:36:35 -0500

    add dependencies

--- api/programa/controllers/programa.js
+++ api/programa/controllers/programa.js
@@ -4,6 +4,7 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
     async find(ctx) {
