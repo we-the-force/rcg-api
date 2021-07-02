b6a1388a1ce088f40ddb7b3db430a0c4b668f162
srlechuga18 <aledleon2@gmail.com>
2021-07-02 12:31:40 -0500

    test to no limit

--- api/programa/controllers/programa.js
+++ api/programa/controllers/programa.js
@@ -5,4 +5,21 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async find(ctx) {
+        let entities;
+        ctx.query = {
+        ...ctx.query,
+        _limit: -1,
+        };
+        if (ctx.query._q) {
+        entities = await strapi.services.programa.search(ctx.query);
+        } else {
+        entities = await strapi.services.programa.find(ctx.query);
+        }
+
+        return entities.map((entity) =>
+        sanitizeEntity(entity, { model: strapi.models.programa })
+        );
+    },
+};
